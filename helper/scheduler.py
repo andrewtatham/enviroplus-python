ea55060974c7d29c96414a8b07a7395cd6c2b22d
Andrew Tatham <andrewtatham@gmail.com>
2021-11-24 10:18:44 +0000

    temperature adustment

--- helper/scheduler.py
+++ helper/scheduler.py
@@ -128,9 +128,9 @@ class MyScheduler:
         elif is_summer:
             target_temperature = 10.0
         elif is_autumn:
-            target_temperature = 14.0
+            target_temperature = 17.0
         elif is_winter:
-            target_temperature = 16.0
+            target_temperature = 18.0
 
         if is_winter:
             if is_early_morning:
