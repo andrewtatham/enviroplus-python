25dd6debee3e1aee51768236f00f4cfb1748385d
Andrew Tatham <andrewtatham@gmail.com>
2022-03-30 14:08:28 +0100

    temp

--- helper/scheduler.py
+++ helper/scheduler.py
@@ -129,15 +129,15 @@ class MyScheduler:
         logging.info('temperature: {}'.format(temperature))
 
         if is_spring:
-            target_temperature = 17.0
+            target_temperature = 18.0
         elif is_summer:
             target_temperature = 10.0
         elif is_autumn:
-            target_temperature = 17.0
+            target_temperature = 18.0
         elif is_winter:
-            target_temperature = 17.0
+            target_temperature = 18.0
         else:
-            target_temperature = 17.0
+            target_temperature = 18.0
 
         if is_winter:
             # if is_early_morning:
