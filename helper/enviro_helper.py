8b39c91a596a23bb24b5750897c2197fbe37bcdb
Andrew Tatham <andrewtatham@gmail.com>
2025-09-12 17:05:58 +0100

    solver

--- helper/enviro_helper.py
+++ helper/enviro_helper.py
@@ -61,7 +61,7 @@ class EnviroWrapper:
 
         # Tuning factor for compensation. Decrease this number to adjust the
         # displayed temperature down, and increase to adjust up
-        self.factor = 1.35
+        self.factor = 1.333
 
         # Create a values dict to store the data
         self.variables = ["temperature",
